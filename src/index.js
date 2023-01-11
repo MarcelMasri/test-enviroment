6d0fdf7735eaf5b1fa90f73f3b59e8483a9744b7
modrnMedia <masri@modrn.de>
2023-01-11 20:32:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 window.addEventListener('load', function(){
-
+  document.querySelectorAll('.review_list-item').forEach(elem => function(elem){
+    const stars = parseInt(elem.querySelector('.review_item-star-count').textContent);
+    const wrapper = elem.querySelector('.review_item-star-wrapper');
+    const star = wrapper.querySelector('.review_item-star');
+    for(let i=0;i<stars;i++){
+      wrapper.appendChild(star.cloneNode());
+    }
+  })
 })
